d7b430a96654bf763c609373b0fee6fae72d42f6
graydon james <graydonj2@gmail.com>
2023-01-05 21:02:26 -0500

    Pseudo code added

--- script.js
+++ script.js
@@ -84,3 +84,79 @@ genreList.listGenres = () => {
 // ask user for input on whether or not they want to keep the given rec or ask for a new one.
 
 // STRETCH: add recs to watchlist
+
+// movieRecApp {
+//      recommendation {
+//          genre
+//          spoken language
+//          release date
+//          lead actor
+//          supporting actor
+//          director
+//          runtime
+//          critical rating
+//          how to watch (ie. netflix, prime, hulu, somewhere for free, etc.)
+//      }
+//      list of genres (array)
+//      list of spoken languages (array)
+//      semi-random list of actors (primary & supporting)
+//      semi-random list of directors
+//      curOptions (array)
+// }
+
+//////////////////////////////////////////////////////////////////////////
+// PSEUDO CODE!
+//////////////////////////////////////////////////////////////////////////
+
+// 1. make namespace object (movieRecApp)
+// 2. initialize API
+// 3. fetch requests for data we want from API
+//      3.1 genres
+//      3.2 dozen random lead actor names
+//      3.3 dozen random supporting actor names
+//      3.4 dozen random director names
+// 4. create init function
+//      4.1 call welcome page (big ol' button: "Get Me My Recommendation, Robot Butler!")
+//      4.2 call genre question page
+//          4.2.1 display options
+//              4.2.1.1 looks at curOptions
+//              4.2.1.2 creates HTML for each option, iteratively
+//              4.2.1.3 displays HTML (append to certain area of website)
+//              4.2.1.4 add button response options
+//              4.2.1.5 wait for response
+//          4.2.2 capture response
+//          4.2.3 store that in movieRecApp->recommendation object
+//      4.3 call release date question page (Now Playing, Recent Release, Classic Film, etc)
+//          4.3.1 display options
+//          4.3.2 capture response
+//          4.3.3 store that in movieRecApp->recommendation object
+//      4.4 call spoken language question page
+//          4.4.1 display options
+//          4.4.2 capture response
+//          4.4.3 store that in movieRecApp->recommendation object
+//      4.5 call critical rating page
+//          4.5.1 display options
+//          4.5.2 capture response
+//          4.5.3 store that in movieRecApp->recommendation object
+//      4.6 call runtime page
+//          4.6.1 display options
+//          4.6.2 capture response
+//          4.6.3 store that in movieRecApp->recommendation object
+//      4.7 call how-to-watch page
+//          4.7.1 display options
+//          4.7.2 capture response
+//          4.7.3 store that in movieRecApp->recommendation object
+//      4.8 call lead actor page
+//          4.8.1 display options
+//          4.8.2 capture response
+//          4.8.3 store that in movieRecApp->recommendation object
+//      4.9 call director page
+//          4.9.1 display options
+//          4.9.2 capture response
+//          4.9.3 store that in movieRecApp->recommendation object
+//      4.10 call supporting actor page
+//          4.10.1 display options
+//          4.10.2 capture response
+//          4.10.3 store that in movieRecApp->recommendation object
+// 5. Fetch from Movie Recommendations API with user input data from movieRecApp->recommendation
+// 6. Present: Title / Tag Line / Poster / Cast / Director / Rating / Overview / Streaming Service (or !IN THEATRES NOW!)
