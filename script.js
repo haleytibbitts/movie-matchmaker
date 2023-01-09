2bdc0f04349cee7bb06211acc1503e6dad96f60b
haleytibbitts <haleytibbitts@gmail.com>
2023-01-08 22:02:35 -0500

    added welcome page html, fetched genre, actor, and director data from API and stored in designated arrays

--- script.js
+++ script.js
@@ -2,73 +2,73 @@
 // https://www.themoviedb.org/settings/api/people
 
 // Fetch genres from API and store within object
-const genreList = {};
-genreList.genres = [];
-fetch(
-  "https://api.themoviedb.org/3/genre/movie/list?api_key=460ad8448635789cb7af9acdaa3d45f2"
-)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (json) {
-    genreList.genres = json.genres;
-    genreList.listGenres();
-  });
+// const genreList = {};
+// genreList.genres = [];
+// fetch(
+//   "https://api.themoviedb.org/3/genre/movie/list?api_key=460ad8448635789cb7af9acdaa3d45f2"
+// )
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (json) {
+//     genreList.genres = json.genres;
+//     genreList.listGenres();
+//   });
 
 // Create function that populates the page with genre question and answers.
-genreList.listGenres = () => {
-  const questionForm = document.querySelector("form");
-  const questionLegend = document.createElement("legend");
-  questionLegend.innerText = "Which genres do you like?";
-  questionForm.appendChild(questionLegend);
+// genreList.listGenres = () => {
+//   const questionForm = document.querySelector("form");
+//   const questionLegend = document.createElement("legend");
+//   questionLegend.innerText = "Which genres do you like?";
+//   questionForm.appendChild(questionLegend);
 
-  // Loop through genres and populate form with labels and inputs with appropriate attributes for each option.
-  genreList.genres.forEach((item) => {
-    // console.log(item);
-    const inputLabel = document.createElement("label");
-    const inputElement = document.createElement("input");
-    inputElement.type = "checkbox";
-    inputElement.id = item.name;
-    inputElement.value = item.name;
-    inputLabel.htmlFor = item.name;
-    inputLabel.innerText = item.name;
-    questionForm.appendChild(inputLabel);
-    questionForm.appendChild(inputElement);
-  });
+// Loop through genres and populate form with labels and inputs with appropriate attributes for each option.
+// genreList.genres.forEach((item) => {
+//   // console.log(item);
+//   const inputLabel = document.createElement("label");
+//   const inputElement = document.createElement("input");
+//   inputElement.type = "checkbox";
+//   inputElement.id = item.name;
+//   inputElement.value = item.name;
+//   inputLabel.htmlFor = item.name;
+//   inputLabel.innerText = item.name;
+//   questionForm.appendChild(inputLabel);
+//   questionForm.appendChild(inputElement);
+// });
 
-  // Create event listener that stores movie suggestions in an object on submit.
-  const submitButton = document.querySelector("button");
-  submitButton.addEventListener("submit", function (e) {
-    e.preventDefault();
-    const movieList = {};
-    movieList.results = [];
-    console.log("clicked");
-    questionForm.children.forEach((genre) => {});
-    const url = new URL(
-      "https://api.themoviedb.org/3/discover/movie?api_key=460ad8448635789cb7af9acdaa3d45f2"
-    );
-    // url.search = new URLSearchParams(with_genres: genreSelection);
-    fetch(url)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (json) {
-        // console.log(json);
-        movieList.results = json.results;
-        movieList.displayMovies();
-      });
+// Create event listener that stores movie suggestions in an object on submit.
+// const submitButton = document.querySelector("button");
+// submitButton.addEventListener("submit", function (e) {
+//   e.preventDefault();
+//   const movieList = {};
+//   movieList.results = [];
+//   console.log("clicked");
+//   questionForm.children.forEach((genre) => {});
+//   const url = new URL(
+//     "https://api.themoviedb.org/3/discover/movie?api_key=460ad8448635789cb7af9acdaa3d45f2"
+//   );
+// url.search = new URLSearchParams(with_genres: genreSelection);
+//     fetch(url)
+//       .then(function (response) {
+//         return response.json();
+//       })
+//       .then(function (json) {
+//         // console.log(json);
+//         movieList.results = json.results;
+//         movieList.displayMovies();
+//       });
 
-    movieList.displayMovies = () => {
-      const page = document.querySelector("body");
+//     movieList.displayMovies = () => {
+//       const page = document.querySelector("body");
 
-      movieList.results.forEach((item) => {
-        const newPara = document.createElement("p");
-        newPara.innerHTML = `<h3>${item.original_title}:</h3> ${item.overview}`;
-        page.appendChild(newPara);
-      });
-    };
-  });
-};
+//       movieList.results.forEach((item) => {
+//         const newPara = document.createElement("p");
+//         newPara.innerHTML = `<h3>${item.original_title}:</h3> ${item.overview}`;
+//         page.appendChild(newPara);
+//       });
+//     };
+//   });
+// };
 
 // make namespace object for our app (movieRecApp)
 
@@ -109,6 +109,95 @@ genreList.listGenres = () => {
 //////////////////////////////////////////////////////////////////////////
 
 // 1. make namespace object (movieRecApp)
+const movieRecApp = {};
+
+// Fetch parameters
+movieRecApp.url = "https://api.themoviedb.org/3/";
+movieRecApp.apiKey = "460ad8448635789cb7af9acdaa3d45f2";
+
+// Object to populate with user info
+// movieRecApp.recommendation = {
+//   genre: userGenre,
+//   lang: userLang,
+//   release: userRelease,
+//   rating: userRating,
+//   runTime: userRunTime,
+//   service: userService,
+//   actor: userActor,
+//   director: userDirector,
+// };
+
+// Arrays to store fetched data from api
+movieRecApp.genreList = [];
+movieRecApp.langList = [];
+movieRecApp.actorList = [];
+movieRecApp.directorList = [];
+
+movieRecApp.getGenreData = () => {
+  const url = new URL(`${movieRecApp.url}genre/movie/list`);
+  url.search = new URLSearchParams({
+    api_key: movieRecApp.apiKey,
+  });
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((jsonResponse) => {
+      // console.log(jsonResponse);
+      movieRecApp.genreList = jsonResponse.genres;
+    });
+};
+
+movieRecApp.getActorData = () => {
+  const url = new URL(`${movieRecApp.url}person/popular`);
+  url.search = new URLSearchParams({
+    api_key: movieRecApp.apiKey,
+  });
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((jsonResponse) => {
+      jsonResponse.results.forEach((e) => {
+        if (e.known_for_department === "Acting") {
+          movieRecApp.actorList.push(e);
+        }
+      });
+    });
+};
+
+movieRecApp.getDirectorData = () => {
+  const url = new URL(`${movieRecApp.url}person/popular`);
+  for (let i = 1; i <= 200; i++) {
+    url.search = new URLSearchParams({
+      api_key: movieRecApp.apiKey,
+      page: i,
+    });
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((jsonResponse) => {
+        jsonResponse.results.forEach((e) => {
+          if (e.known_for_department === "Directing") {
+            movieRecApp.directorList.push(e);
+          }
+        });
+      });
+    if (movieRecApp.directorList.length > 4) {
+      break;
+    }
+  }
+};
+
+// Init function
+movieRecApp.init = () => {
+  movieRecApp.getGenreData();
+  movieRecApp.getActorData();
+  movieRecApp.getDirectorData();
+  console.log(movieRecApp.directorList);
+};
+
 // 2. initialize API
 // 3. fetch requests for data we want from API
 //      3.1 genres
@@ -160,3 +249,4 @@ genreList.listGenres = () => {
 //          4.10.3 store that in movieRecApp->recommendation object
 // 5. Fetch from Movie Recommendations API with user input data from movieRecApp->recommendation
 // 6. Present: Title / Tag Line / Poster / Cast / Director / Rating / Overview / Streaming Service (or !IN THEATRES NOW!)
+movieRecApp.init();
